8070e9b897728ef0ca6581bc05e01e6b231efb78
Bieldeid <gabrieldeid.android@gmail.com>
2024-10-21 21:50:22 -0300

    :hammer: Corrigindo service e repository Users, controller faltando

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -14,14 +14,6 @@ const transporter =  nodemailer.createTransport({
     },
 });
 
-const contato = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'click.studio.ilustration@gmail.com',
-      pass: 'uoueupvqdobqlubg',
-    },
-  });
-
 module.exports = {
 
     /*
@@ -30,214 +22,103 @@ module.exports = {
     CLIENTE = 3
     */
 
-    async getAllUsers(user){
-        try{
+    async getAllUsers(){
             const user = await db('user').select('*');
             return {message: user};
-        } catch (err){
-            console.error('Usuários não encontrados', err);
-            return {message: 'Usuários não encontrados.'}
-        }
     },
 
-    async createUser(user){
+    async getEspecifUserByEmail(email){
+        const user = await db('user').select('id').where({email}).first();
+        return user;
+    },
+
+    async resultEmail (user){
+        const{email} = user;
+        const resultEmail = await db ('user').where('email',email).select('*')
+        return resultEmail
+    },
+
+    async resultCpf(user){
+        const{CPF} = user;
+        const resultCpf = await db('user').where('CPF', CPF).select('*')
+        return resultCpf
+    },
+
+    async createUser(user, hashPass, date){
         const{nome,telefone,email,CPF,CEP,senha,role} = user;
-        
-        try{
-            const resultEmail = await db('user').where('email',email).select('*');
-            const resultCpf = await db('user').where('CPF', CPF).select('*');
+        const [id] = await db('user').insert({
+            nome,
+            telefone,
+            email,
+            CPF,
+            CEP,
+            role,
+            senha: hashPass,
+            dataEntrada: date
+        });
 
-            if (resultEmail.length || resultCpf.length > 0){
-                console.error('Usuario já cadastrado');
-                return {message: 'Não é possível cadastrar usuario, CPF ou Email já existentes.'};
-            }
-
-            const hashedPassword = await bcrypt.hash(senha, saltRounds);
-            const dataAtual = moment().format('YYYY-MM-DD');
-
-            console.log("Criando usuario")
-
-            const [id] = await db('user').insert({
-                nome,
-                telefone,
-                email,
-                CPF,
-                CEP,
-                role,
-                senha : hashedPassword,
-                dataEntrada : dataAtual
-            });
-
-            if (role === "Fotografo"){
-                await db('fotografo').insert({ idUser: id });
-                return {id, message: 'Fotografo criado com sucesso!'}
-            }
-
-            if(role === "Cliente"){
-                await db('cliente').insert({ idUser: id });
-                return {id, message: 'Cliente criado com sucesso!'}
-            }
-
-            return {id, message: 'Administrador criado com sucesso!'}
-            
-           
-            
-        } catch(err){
-            console.error('Erro ao cadastrar usuário', err);
-            return {message: 'Não foi possível cadastrar o usuário.'};
-        }
+        return id;
     },
+  
 
     async updateUser (user, idUser){
-        const {id} = idUser;
-        const {nome, telefone, email, CEP} = user;
-
-        try{
-            await db('user').where({id}).update({
+        const {id} = idUser; //Puxa o ID
+        const {nome, telefone, email, CEP} = user; //Puxa todas as informações
+        await db('user').where({id}).update({
                 nome,
                 telefone,
                 email,
                 CEP
-            });
-            return{id,message: 'Usuário atualizado'};
-        }catch(err){
-            console.error('Erro ao atualizar usuário', err)
-            return {message: 'Não foi possível atualizar o usuário.'};
-        }
+        }); //Faz update no banco
     },
     
     async deleteUser(idUser){
         const {id} = idUser;
-
-        try{
             //await db('jobs').where({idUser: id}).del();
-            await db('user').where({id}).del();
-            
-            return{message: 'Usuário excluído com sucesso!'}
-
-        }catch(err){
-            console.error('Houve um erro ao excluir o usuário: ', err)
-            throw new Error('Não foi possível excluir o usuário.');
-        }
+            await db('user').where({id}).del();         
     },
 
-    async reqPasswordReset (userEmail) {
-        const {email} = userEmail;
-        try {
-            const resultUser = await db('user')
-            if (resultUser.length > 0) {
-                const user = await db('user').select('id').where({email}).first();
-                const idUser = user.id;
+    async validTicket(ticket){
+        const validTicket = await db('password_reset_tickets').select('ticket').where({ ticket }).first();;
+        return validTicket;
+    },
 
-                let ticket;
-                let validTicket ;
+    async validEmail(userEmail){
+        const resultEmail = await db('user').select('id').where({email}).first();
+        return resultEmail;
+    },
 
-                do{
-                    ticket = crypto.randomBytes(4).toString('hex');
-                    validTicket = await db('password_reset_tickets').select('ticket').where({ ticket }).first();;
-                }while (validTicket)
-                
-                await db('password_reset_tickets').insert({
+
+    async reqPasswordReset (idUser,ticket) {      
+        await db('password_reset_tickets').insert({
                     idUser,
                     ticket
-                });
-
-                 await transporter.sendMail({
-                    from: '',
-                    to: email,
-                    subject: 'Redefinição de Senha',
-                    html: `
-                        <div style="background-color: black; padding: 8px 20px; text-align: center;">
-                                <h2 style="font-size: 24px; color: #fff; font-family: 'Baloo', sans-serif; font-weight: 700;">Click</h2>
-                            </div>
-                            <div style="padding: 20px; background-color: white;">
-                                <p style="font-size: 16px; color: black;">Olá!</p>
-                                <p style="font-size: 16px; color: black;">Esse é seu <strong style="color: black;">Código</strong> para redefinir a sua senha: ${token}!</p>
-                                <p style="font-size: 16px; color: black;"><strong style="color: black;">Click</strong> está à disposição. :)</p>
-                        </div>`,
-                 });
-
-                return {message: 'Código de redefinição de senha enviado'};
-            }
-
-            console.log("Email não cadastrado")
-            return {message: 'Email não cadastrado'};
-
-        }catch(err){
-            console.error('Erro ao verificar email existente', err);
-            throw new Error('Não foi possível verificar o email.')
-
-        }
-
+        });
     },
 
     async verifyPasswordResetTicket(userTicket){
         const {ticket} = userTicket
-
-        try{
-            const result = await db('password_reset_tickets').where({ ticket}).select('*');
+        const result = await db('password_reset_tickets').where({ ticket}).select('*');
 
             if (result.length < 1){
                 console.log("Ticket inválido! " + result);
                 return{message: 'Ticket inválido! Favor digitar novamente!'};
             }
 
-            await db('password_reset_tickets').where({ticket}).del();
-            return {message: 'Ticket validado!'}
-        } catch(err){
-            console.error("Erro na validação de ticket: ", err)
-            return {message: 'Não foi possível validar o ticket.'}
-        }
     },
 
-    async resetPassword(user){
-        const {email, senha, confirmaSenha} = user;
+    async deletePassResetTicket(ticket){
+        await db('password_reset_tickets').where({ticket}).del();
+    },
 
-        try{
-            if (senha != confirmaSenha){
-                return {message: 'Senhas diferentes!'}
-            }
-
-            const hashedPass = await bcrypt.hash(senha, saltRounds);
-            await db('user').where({email: email}).update({senha: hashedPass})
-            return {message: 'Senha alterada com sucesso!'}
-        } catch(err){
-            console.error("Erro ao redefinir senha: ",err);
-            return {message: 'Erro ao redefinir a senha!'}
-        }
-
+    async resetPassword(user, hashedPass){
+        const {email} = user;
+        await db('user').where({email: email}).update({senha: hashedPass})
     },
 
     async authLogin(authUser, res){
         const {email, senha} = authUser;
-
-        try{
-            const user = await db('user').where({email}).select('*');
-
-            if (user.length === 0){
-                return {message: 'Email inválido!'};
-            }
-
-            const encryptedPassword = user[0].senha;
-
-            const comparePassword = await bcrypt.compare(senha, encryptedPassword)
-
-            if (!comparePassword){
-                return {auth: false, message: "Login inválido"}
-            }
-
-            const token = jwt.sign({id: user[0].id}, 'your_secret_key', { expiresIn: '1h' });
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                maxAge: 3600000 
-            });
-
-            return {auth:true, message: 'Login realizado com sucesso!'}
-
-        } catch(err){
-            console.log(err);
-            return {auth: false, message: 'Falha ao realizar o login!'}
-        }
+        const user = await db('user').where({email}).select('*');
+        return user;
     }
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,15 @@
 const UserRepository = require('../repositories/userRepository')
+const crypto = require('crypto');
+
+const saltRounds = 10;
+
+const transporter =  nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'click.studio.ilustration@gmail.com',
+        pass: 'uoueupvqdobqlubg',
+    },
+});
 
 module.exports = {
     async getAllUsers(){
@@ -6,11 +17,34 @@ module.exports = {
     },
 
     async createUser(user){
-        return await UserRepository.createUser(user)
+            const resultEmail =  await UserRepository.resultEmail(user)
+            const resultCpf = await UserRepository.resultCpf(user)
+
+            if(resultEmail.length > 0 || resultCpf.length > 0){
+                return {message: 'Email ou CPF já cadastrados'}
+            }
+            
+            const hashPass = await bcrypt.hash(user.senha, saltRounds)
+            const dataAtual = moment().format('YYYY-MM-DD');
+
+            const id = await UserRepository.createUser(user, dataAtual, hashPass)
+
+            if (role === "Fotografo"){
+                return {id, message: 'Fotografo criado com sucesso!'}
+            } 
+
+            if(role === "Cliente"){
+                return {id, message: 'Cliente criado com sucesso!'}
+            }
+
+            return {id, message: 'Administrador criado com sucesso!'}
+
+
     },
 
     async updateUser(user,idUser){
-        return await UserRepository.updateUser(user,idUser)
+             await UserRepository.updateUser(user,idUser)
+             return {message: 'Usuário atualizado com sucesso!'}     
     },
 
     async deleteUser(idUser){
@@ -18,18 +52,84 @@ module.exports = {
     },
 
     async reqPasswordReset(userEmail){
-        return await UserRepository.reqPasswordReset(userEmail)
+        const resultEmail = await UserRepository.validEmail(userEmail);
+        
+        if(resultEmail.length === 0){
+            return {message: 'Email não cadastrado'}
+        }
+
+        let ticket
+        let validTicket
+
+        do{
+            ticket = crypto.randomBytes(4).toString('hex');
+            validTicket = await UserRepository.validTicket(ticket);
+        } while (validTicket)
+
+        const idUser = await UserRepository.getEspecifUserByEmail(resultEmail);
+        await UserRepository.reqPasswordReset(idUser, ticket);
+
+
+        await transporter.sendMail({
+            from: '',
+            to: email,
+            subject: 'Redefinição de Senha',
+            html: `
+                 <div style="background-color: black; padding: 8px 20px; text-align: center;">
+                    <h2 style="font-size: 24px; color: #fff; font-family: 'Baloo', sans-serif; font-weight: 700;">Click</h2>
+                </div>
+                    <div style="padding: 20px; background-color: white;">
+                        <p style="font-size: 16px; color: black;">Olá!</p>
+                        <p style="font-size: 16px; color: black;">Esse é seu <strong style="color: black;">Código</strong> para redefinir a sua senha: ${token}!</p>
+                        <p style="font-size: 16px; color: black;"><strong style="color: black;">Click</strong> está à disposição. :)</p>
+                </div>`,
+            });
+        
+            return{message: 'Ticket para reset de senha enviado para o email'}
     },
 
     async verifyPasswordResetTicket(userTicket){
-        return await UserRepository.verifyPasswordResetTicket(userTicket)
+        const resultTicket = await UserRepository.verifyPasswordResetTicket(userTicket);
+        if (resultTicket.length < 1){
+            console.log("Ticket inválido! " + userTicket);
+            return{message: 'Ticket inválido! Favor digitar novamente!'};
+        }
+
+        await UserRepository.deletePasswordResetTicket(userTicket);
+        return{message: 'Ticket válido'}
     },
 
     async resetPassword(user){
-        return await UserRepository.resetPassword(user)
+        const {senha, confirmaSenha} = user;
+        if (senha != confirmaSenha){
+            return {message: 'Senhas diferentes!'}
+        }
+        const hashedPass = await bcrypt.hash(senha, saltRounds);
+        return await UserRepository.resetPassword(user, hashedPass)
     },
 
-    async authLogin(authUser, res){
-        return await UserRepository.authLogin(authUser, res)
+    async authLogin(user, res){
+        const {email,senha} = user;
+
+        const validUser = await UserRepository.resultEmail(user);
+        if(validUser.length === 0){
+            return {message: 'Email ou senha inválido!'};
+        }
+
+        const encryptedPassword = validUser[0].senha;
+        const comparePassword = await bcrypt.compare(senha, encryptedPassword)
+
+        if(!comparePassword){
+            return {auth: false, message: "Login inválido"}
+        }
+
+        const token = jwt.sign({id: user[0].id}, 'your_secret_key', { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 3600000 
+            });
+
+        return {auth:true, message: 'Login realizado com sucesso!'}
     }
 }
