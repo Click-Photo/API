1e67ce90ca2afc4adbbe22e54084649d0112c0d2
Beatriz-Chagas <bchagas440@gmail.com>
2024-11-03 02:20:36 -0300

    🎨 Repository Patterns Propostas

--- src/repositories/propostaRepository.js
+++ src/repositories/propostaRepository.js
@@ -1,62 +1,27 @@
 const db = require('../database/db');
 const nodemailer = require('nodemailer');
 
-module.exports = {
-    async getAllPropostas (proposta){
-        try{
-            const propostas = await db('proposta').select('*')
-            return{message: propostas}
-        } catch(err){
-            console.error(err)
-            return{message: 'Proposta não encontradas'}
-        }
-    },
+class PropostaRepository {
+    async getAllPropostas() {
+        return await db('proposta').select('*');
+    }
 
-    async createProposta(proposta, idJobs){
-        const{
-            idFotografo,
+    async createProposta(idJobs, idCliente, idFotografo) {
+        return await db('proposta').insert({
             idCliente,
-        } = proposta;
-        try{
-            const validarJob = await db('jobs').select('status').where('id', idJobs)
-            const statusJob = validarJob[0].status
+            idFotografo,
+            idJobs
+        });
+    }
 
-            if(statusJob == "Aceito" || statusJob == "Finalizado"){
-                return {message: "Job não pode mais receber propostas."}
-            }
+    async deleteProposta(id) {
+        await db('proposta').where({ id }).del();
+    }
 
-            const {id} = await db('proposta')
-            insert({
-                idJobs,
-                idCliente,
-                idFotografo
-            })
-
-            return {message: "Proposta realizada"}
-        } catch(err){
-            console.error(err);
-            return {message: "Erro ao realizar a proposta"}
-        }
-    },
-
-    async deleteProposta(proposta){
-        const {id} = proposta;
-        try{
-            await db('proposta').where('id', id).del();
-            return {message: "Proposta excluída"}
-        } catch(err){
-            console.error(err);
-            return {message: "Erro ao excluir a proposta"}
-        }
-    },
-
-    async getAllPropostaCliente(id){
-        const {idCliente} = id;
-
-        try{
-            const propostas = await db('proposta')
+    async getAllPropostaCliente(idCliente) {
+        return await db('proposta')
             .join('jobs', 'proposta.idJobs', 'jobs.id')
-            .join('fotografo', 'proposta.idFotografo', 'fotografo.id')
+            .join('user', 'proposta.idFotografo', 'user.id')
             .select(
                 'proposta.id',
                 'proposta.idJobs',
@@ -65,85 +30,66 @@ module.exports = {
                 'proposta.status',
                 'jobs.dataJob',
                 'jobs.preco',
-                'fotografo.nome as nomeFotografo'
+                'user.nome as nomeFotografo'
             )
-            .where({'proposta.idCliente': idCliente});
-
-            const totalPropostasPorJob = await db('proposta')
-                .select('idJobs')
-                .count('idJobs as totalPropostas')
-                .groupBy('idJobs')
-                .where({ idCliente: idCliente });
-    
-            // Mapeia as propostas incluindo a contagem de propostas para cada job
-            const propostasComContagem = propostas.map(proposta => {
-                const contagem = totalPropostasPorJob.find(tp => tp.idJobs === proposta.idJobs);
-                return { ...proposta, totalPropostas: contagem ? contagem.totalPropostas : 0 };
-            });
-
-            return{propostasComContagem}
-        } catch(err){
-            console.error(err);
-            return {message: "Erro ao buscar propostas."}
-        }
-    },
-
-    async getPropostaJob(id){
-        const {idJobs} = id;
-
-        try{
-            const propostas = await db('proposta')
-                .join('jobs', 'proposta.idJobs', 'jobs.id')
-                .join('fotografo', 'proposta.idFotografo', 'fotografo.id')
-                .select(
-                    'proposta.id',
-                    'proposta.idJobs',
-                    'proposta.idCliente',
-                    'proposta.idFotografo',
-                    'proposta.status',
-                    'jobs.dataJob',
-                    'jobs.preco',
-                    'fotografo.nome as nomeFotografo'
-                )
-                .where({ 'proposta.idJobs': idJob })
-                .andWhere(builder => {
-                    builder.whereNot({ 'proposta.status': 'Recusado' })
-                           .orWhereNull('proposta.status');
-                });
-    
-            return{propostas};
-        } catch(err){
-            console.error(err);
-            return{ message: "Erro ao buscar propostas" };
-        }
-    },
-
-    async getAllPropostaFotografo(id){
-        const {idFotografo} = id;
-
-        try{
-            const proposta = await db("proposta").select("*").where({idFotografo:idFotografo});
-            return {proposta}
-        }catch(err){
-            console.error(err);
-            return {message: "Erro ao buscar propostas"}
-        }
-    },
-
-    async getAllPropostaJob(id){
-        const {idJobs} = id;
-        try{
-            const validarJob = await db('jobs').select('status').where('id',idJobs)
-            const statusJob = validarJob[0].status
-
-            if(statusJob == "Aceito"){
-                return {message: "Job com status aceito. "}
-            }
-        } catch(err){
-            console.error(err);
-            return {message: "Erro ao validar status do job"}
-        }
+            .where({ 'proposta.idCliente': idCliente });
     }
 
+    async getPropostaJob(idJob) {
+        return await db('proposta')
+            .join('jobs', 'proposta.idJobs', 'jobs.id')
+            .join('user', 'proposta.idFotografo', 'user.id')
+            .select(
+                'proposta.id',
+                'proposta.idJobs',
+                'proposta.idCliente',
+                'proposta.idFotografo',
+                'proposta.status',
+                'jobs.dataJob',
+                'jobs.preco',
+                'user.nome as nomeFotografo'
+            )
+            .where({ 'proposta.idJobs': idJob })
+            .andWhere(builder => {
+                builder.whereNot({ 'proposta.status': 'Recusado' })
+                    .orWhereNull('proposta.status');
+            });
+    }
 
-}
+    async getAllPropostaFotografo(idFotografo) {
+        return await db('proposta').select('*').where({ idFotografo });
+
+    }
+
+    async getAllPropostaJob(idJobs) {
+        return await db('proposta').select('*').where('idJobs', idJobs);
+    }
+
+    async aceitarProposta(id, idJobs, idFotografo) {
+        await db('jobs').where('id', idJobs).update({
+            status: 'Aceito',
+            idFotografo
+        });
+        await db('proposta').where({ id }).update({
+            status: 'Aceito'
+        });
+    }
+
+    async recusarProposta(id) {
+        await db('proposta').where({ id }).update({
+            status: 'Recusado'
+        });
+    }
+
+    async sendEmail(email, subject, htmlContent) {
+        await transporter.sendMmail({
+            from: '',
+            to: email,
+            subject: subject,
+            html: htmlContent
+
+        });
+    }
+};
+
+module.exports = new PropostaRepository();
